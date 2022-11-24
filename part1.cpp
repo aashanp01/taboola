858b3250ad81286edbf1ac7b3c1bbb1a4b157c51
aashanp01 <91083059+aashanp01@users.noreply.github.com>
2022-11-24 00:24:26 -0500

    Create part1.cpp

---
+++ part1.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <cstdlib>
+#include <unordered_map>
+#include <algorithm>
+
+//Part 1
+class Obj
+{
+    //incomplete
+};
+void process(string json)
+{
+    unordered_map<strin,obj> mp;
+    istringstream str(json);
+    string predec, word, proced;
+    str>>word; // get initial bracket
+    unordered_map<string,string> mp;
+    while(str>>word)
+    {
+        if(word == ":")
+        {
+            str>>proced;
+            if(proced == "{")
+            {
+                
+            }
+        }
+        predec = word;
+    }
+}
